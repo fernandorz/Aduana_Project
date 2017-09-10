d0e15efbbe7a93ea6deee531f208b1af10fc59bf
Jose <MSanchez@email.com>
2017-09-10 09:02:21 -0600

    Comenzo versionamiento

---
+++ Aduana_app/Aduana_app/ws_helloWorld.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Aduana_app
+{
+    /// <summary>
+    /// Summary description for ws_helloWorld
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ws_helloWorld : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
